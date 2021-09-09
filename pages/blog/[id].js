06e596b6e8202755d1c7222e23d5b3d0481c203e
Ashutosh Singh <ashu9993063472@gmail.com>
2021-09-09 23:58:20 +0530

    blog rendering feature added

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -10,7 +10,10 @@ import SwiperNavigation from '@components/swiper/NavButton';
 import SwiperSection from '@components/swiper/SwiperSection';
 import PageWrapper from '../../components/layout/PageWrapper';
 
-function BlogPage() {
+import { getAllBlogs, getBlog } from '@utils/api';
+import BlogMarkDown from '@components/BlogMarkDown';
+
+function BlogPage({ blog, blogs }) {
   const prevRef = useRef(null);
   const nextRef = useRef(null);
   const [loadCommentBtn, setLoadCommentBtn] = useState(true);
@@ -33,99 +36,23 @@ function BlogPage() {
   return (
     <PageWrapper>
       <Section>
-        <div className="blog__container text-gray-600 mb-10">
-          <Typography type="secondary" className="text-gray-700">
-            Why you should join us?
-          </Typography>
-          <Typography type="content" className="my-5">
-            Wired is a <span className="highlight">massively</span> popular
-            publication, good at capturing real insights into the tech world, no
-            stranger to topics like <span className="bold">technology</span>,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-          <div>
-            <CustomImageComponent
-              src="/assets/images/tech.jpg"
-              className="blog__poster"
-            />
-          </div>
-
-          <Typography type="content" className="my-5">
-            Wired is a massively popular publication, good at capturing real
-            insights into the tech world, no stranger to topics like technology,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-
-          <Typography type="content" className="my-5">
-            Wired is a massively popular publication, good at capturing real
-            insights into the tech world, no stranger to topics like technology,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-
-          <Typography type="section" className="">
-            Features
-          </Typography>
-
-          <Typography type="content" className="my-5">
-            Wired is a <span className="bold"> massively</span> popular
-            publication, <b>good at capturing </b>
-            real insights into the tech world, no stranger to topics like
-            technology, entertainment, science, <b>culture</b>, politics, and
-            social media. Informed and comprehensive, Wired is basically the
-            perfect tech blog to follow.the tech world, no stranger to topics
-            like technology, entertainment, science, culture, politics, and
-            social media. Informed and <a href="/">comprehensive</a>, Wired is
-            basically the perfect tech blog to follow.
-          </Typography>
-
-          <Typography type="section">Perks of DLG</Typography>
-          <Typography className="list-decimal" type="list-item">
-            Testing
-          </Typography>
-          <Typography className="list-decimal" type="list-item">
-            watch money heist
-          </Typography>
-          <Typography className="list-decimal" type="list-item">
-            next season is on december
-          </Typography>
-
-          <Gist id="013c3d1c5385ac253d0af27d06d53f06" />
-          <div>
-            <CustomImageComponent
-              src="/assets/images/tech2.jpg"
-              className="blog__poster"
-            />
-          </div>
-          <Typography
-            className="blockqoute"
-            className="my-10"
-            type="blockquote"
-          >
-            " Nothing in the life is to be feared, it is only to be
-            understood... now it's the time to understand more, so that we can
-            fear less." - Marie Curie
-          </Typography>
+        <div className='blog__container text-gray-600 mb-10'>
+          <BlogMarkDown data={JSON.parse(blog.body)} />
           {loadCommentBtn && (
-            <Button btnType="primary" onClick={loadComments}>
+            <Button btnType='primary' onClick={loadComments}>
               Load Comments
             </Button>
           )}
-          <div id="disqus_thread" className="my-5"></div>
+          <div id='disqus_thread' className='my-5'></div>
         </div>
       </Section>
       <Section>
-        <div className="mb-5 flex items-center justify-between">
+        <div className='mb-5 flex items-center justify-between'>
           <div>
-            <Typography className="text-gray-700" type="secondary">
+            <Typography className='text-gray-700' type='secondary'>
               Related Blogs
             </Typography>
-            <Typography className="text-gray-500" type="header-caption">
+            <Typography className='text-gray-500' type='header-caption'>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
             </Typography>
@@ -133,11 +60,11 @@ function BlogPage() {
           <SwiperNavigation nextRef={nextRef} prevRef={prevRef} />
         </div>
         <SwiperSection
-          data={new Array(9).fill(2)}
+          data={blogs}
           nextRef={nextRef}
           prevRef={prevRef}
           component={BlogCard}
-          className="my-10"
+          className='my-10'
         />
       </Section>
     </PageWrapper>
@@ -145,3 +72,37 @@ function BlogPage() {
 }
 
 export default BlogPage;
+
+export const getStaticPaths = async () => {
+  const result = await getAllBlogs();
+  let data;
+  if (result.status === 'fail') {
+    data = [];
+  } else {
+    data = result.blog;
+  }
+  const paths = data.map((blog) => {
+    return {
+      params: {
+        id: blog._id,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const blog = await getBlog({ id: params.id });
+  const result = await getAllBlogs();
+  const blogs = result.blog;
+  return {
+    props: {
+      blog,
+      blogs: blogs,
+    },
+    revalidate: 3, // Incremental Site Generation
+  };
+};
