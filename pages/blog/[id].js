470f35b05a3cc896ffb33b24e718aa5799222ba1
ajay767 <ajaykumaryadavbina4@gmail.com>
2021-09-09 23:58:51 +0530

    fix

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -3,8 +3,7 @@ import Section from '@layout/Section';
 import BlogCard from '@front/BlogCard';
 import Typography from '@components/Typography';
 import Button from '@components/Button';
-import Gist from 'react-gist';
-import CustomImageComponent from '../../components/Image';
+import BlogMarkdown from '@components/BlogMarkdown';
 
 import SwiperNavigation from '@components/swiper/NavButton';
 import SwiperSection from '@components/swiper/SwiperSection';
@@ -34,83 +33,7 @@ function BlogPage() {
     <PageWrapper>
       <Section>
         <div className="blog__container text-gray-600 mb-10">
-          <Typography type="secondary" className="text-gray-700">
-            Why you should join us?
-          </Typography>
-          <Typography type="content" className="my-5">
-            Wired is a <span className="highlight">massively</span> popular
-            publication, good at capturing real insights into the tech world, no
-            stranger to topics like <span className="bold">technology</span>,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-          <div>
-            <CustomImageComponent
-              src="/assets/images/tech.jpg"
-              className="blog__poster"
-            />
-          </div>
-
-          <Typography type="content" className="my-5">
-            Wired is a massively popular publication, good at capturing real
-            insights into the tech world, no stranger to topics like technology,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-
-          <Typography type="content" className="my-5">
-            Wired is a massively popular publication, good at capturing real
-            insights into the tech world, no stranger to topics like technology,
-            entertainment, science, culture, politics, and social media.
-            Informed and comprehensive, Wired is basically the perfect tech blog
-            to follow.
-          </Typography>
-
-          <Typography type="section" className="">
-            Features
-          </Typography>
-
-          <Typography type="content" className="my-5">
-            Wired is a <span className="bold"> massively</span> popular
-            publication, <b>good at capturing </b>
-            real insights into the tech world, no stranger to topics like
-            technology, entertainment, science, <b>culture</b>, politics, and
-            social media. Informed and comprehensive, Wired is basically the
-            perfect tech blog to follow.the tech world, no stranger to topics
-            like technology, entertainment, science, culture, politics, and
-            social media. Informed and <a href="/">comprehensive</a>, Wired is
-            basically the perfect tech blog to follow.
-          </Typography>
-
-          <Typography type="section">Perks of DLG</Typography>
-          <Typography className="list-decimal" type="list-item">
-            Testing
-          </Typography>
-          <Typography className="list-decimal" type="list-item">
-            watch money heist
-          </Typography>
-          <Typography className="list-decimal" type="list-item">
-            next season is on december
-          </Typography>
-
-          <Gist id="013c3d1c5385ac253d0af27d06d53f06" />
-          <div>
-            <CustomImageComponent
-              src="/assets/images/tech2.jpg"
-              className="blog__poster"
-            />
-          </div>
-          <Typography
-            className="blockqoute"
-            className="my-10"
-            type="blockquote"
-          >
-            " Nothing in the life is to be feared, it is only to be
-            understood... now it's the time to understand more, so that we can
-            fear less." - Marie Curie
-          </Typography>
+         <BlogMarkdown data={} />
           {loadCommentBtn && (
             <Button btnType="primary" onClick={loadComments}>
               Load Comments
